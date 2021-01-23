289f69703ee4b378c16554f98ad2cd1791e60e1d
Sourav <souravg@wheelpros.com>
2021-01-24 03:25:52 +0530

    Final push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/dashboard/home/home.component';
+import { TryComponent } from './components/dashboard/try/try.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component: LoginComponent},
+  {path : 'dashboard', component: DashboardComponent, children:[
+    {path: '',component:HomeComponent},
+    {path: 'try',component:TryComponent}
+  ]}
+  
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { HomeComponent } from './components/dashboard/home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TryComponent } from './components/dashboard/try/try.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    DashboardComponent,
+    TryComponent
   ],
   imports: [
     BrowserModule,
